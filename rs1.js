cfa9c26459d6d4dffa13bfd0886a8432993a460c
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-09-20 15:05:26 +0800

    change json file name

--- rs1.js
+++ rs1.js
@@ -37,63 +37,35 @@ server.listen(port, function (error) {
 
   for (irs = 1; irs < 2; irs++) {
     for (ips = 1000; ips < 1001; ips++) {
-      // console.clear();
-      // console.log("\x1b[32m");
-      // console.log(
-      //   " RS  PS   TS   tick" +
-      //     "   hash code                                                         " +
-      //     "rbas time      " +
-      //     "elapse"
-      // );
-
       for (its = 1000; its < 2000; its++) {
-        // timeNow = Date.now();
-        // timeTake = timeNow - start;
-        // data = "\n"+ hashdisplay+ "  " + hash_data + "  " +timeTake;
-
-        // console.log(
-        //   " " +
-        //     irs +
-        //     "  " +
-        //     hash_data +
-        //     "  " +
-        //     hashdisplay +
-        //     "  " +
-        //     timeNow +
-        //     "  " +
-        //     timeTake
-        // );
-
         k = " " + ips + " " + its + " ";
         for (itc = 10000; itc < 10001; itc++) {
           hash_data = k + itc;
-
           hashdisplay = crypto
             .createHash("sha256")
             .update(hash_data)
             .digest("hex");
+          data =
+            '{"hash":' +
+            '"' +
+            hashdisplay +
+            '",' +
+            ' "from":' +
+            '"' +
+            hash_data +
+            '"},';
+          fs.appendFileSync("hash.json", data);
         }
-        data =
-          '{"hash":' +
-          '"' +
-          hashdisplay +
-          '",' +
-          ' "from":' +
-          '"' +
-          hash_data +
-          '"},';
-        fs.appendFileSync("hash.json", data);
+
       }
     }
   }
-
-
+  fileDisp = fs.readFileSync("hash.json")
+  console.log(fileDisp)
   timeNow = Date.now();
   timeTake = timeNow - start;
   console.log(timeNow);
   console.log(timeTake);
-
   data = "]" + "\n" + start + "  " + timeTake;
-
   fs.appendFileSync("hash.json", data);
 });

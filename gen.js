5a4d4eba3ccd46677c21bdecc45260aaa975154b
KBS-james <37357237+KBS-james@users.noreply.github.com>
2020-10-26 22:21:29 +0800

    load sim init

--- gen.js
+++ gen.js
@@ -1,70 +1,85 @@
-const http = require("http");
 const fs = require("fs");
-const port = 3601;
-const server = http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("rssync.html", function (error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write("Error: File not found");
-    } else {
-      res.write(data);
-    }
-    res.end();
-  });
-});
-var crypto = require("crypto");
+console.clear();
+console.log("\x1b[32m");
+var hash = ""
+time = Date.now()
 
-server.listen(port, async function (error) {
-  const start = Date.now();
-  console.log(start);
-  if (error) {
-    console.log("something wrong", error);
-  } else {
-    console.log(Date.now() - start);
-    console.log("rs1 sync server is listening on port " + port);
-    console.log(Date.now() - start);
+function hexIn(digit) {
+  switch(digit){
+    case 0: return "0"
+    case 1: return "1"
+    case 2: return "2"
+    case 3: return "3"
+
+    case 4: return "4"
+    case 5: return "5"
+    case 6: return "6"
+    case 7: return "7"
+
+    case 8: return "8"
+    case 9: return "9"
+    case 10: return "a"
+    case 11: return "b"
+
+    case 12: return "c"
+    case 13: return "d"
+    case 14: return "e"
+    case 15: return "f"
   }
-  var irs = 10;
-  var ips = 1000;
-  var itc = 10000;
-  var hash_data;
-  var hashdisplay;
-
-  for (ips = 1000; ips < 1010; ips++) {
-    console.clear();
-    console.log("\x1b[32m");
-
-
-    for (its = 1000; its < 1050; its++) {
-    console.log(
-      " RS  PS   TS   tick" +
-        "   hash code                                                         " +
-        "rbas time      " +
-        "elapse"
-    );
-
-
-      for (itc = 100000; itc < 100010; itc++) {
-        hash_data = " " + ips + " " + its + " " + itc;
-        hashdisplay = await crypto
-          .createHash("sha256")
-          .update(hash_data)
-          .digest("hex");
-
-        console.log(
-          " " +
-            irs +
-            " " +
-            hash_data +
-            " " +
-            hashdisplay +
-            "  " +
-            Date.now() +
-            "  " +
-            (Date.now() - start)
-        );
+}
+hash_data = "tc0000"
+hashOut = ""
+ita=0
+itb=0
+itc=0
+itd=0
+ite=0
+itaMax=16
+itbMax=16
+itcMax=16
+itdMax=1
+iteMax=1
+function lasthash()  {
+  // console.log("ita="+ ita + " itb="+itb+" itc="+itc+"itd="+itd)
+  if (ita < (itaMax-1)) return false
+  if (itb < (itaMax-1)) return false
+  if (itc < (itaMax-1)) return false
+  // if (itd < (itaMax-1)) return false
+  // if (ite < (itaMax-1)) return false
+  // console.log("last hash")
+  return true
+}
+data = "[";
+fs.appendFileSync("hash.json", data);
+fs.unlinkSync("hash.json")
+data = "[";
+fs.appendFileSync("hash.json", data);
+for (ita = 0; ita < itaMax; ita++) {
+  for (itb = 0; itb < itbMax; itb++) {
+    for (itc = 0; itc < itcMax; itc++) {
+      for (itd = 0; itd < itdMax; itd++) {
+        for (ite = 0; ite < iteMax; ite++) {
+          hashOut = (hexIn(ita)) + hexIn(itb) + hexIn(itc) + hexIn(itd)+
+          hexIn(ite)+ "00000000000" + "00000000"+"00000000" +
+          "00000000" + "00000000" + "00000000"+"00000000" 
+          // console.log(hashOut)
+          data =
+          '{"hash":' +
+          '"' + hashOut+
+          '",' +
+          ' "from":' +
+          '"' +
+          hash_data +
+          '"}';
+          if (lasthash() == false) {data = data +","}
+          fs.appendFileSync("hash.json", data);
+        }
       }
     }
+
+    };
   }
-});
+  data = "]";
+  fs.appendFileSync("hash.json", data); 
+total = Date.now()
+console.log(total - time)
---
+++ r0PS1000.js
@@ -0,0 +1,51 @@
+// TS simulatoe send 1000 hash to PS per second
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+var crypto = require("crypto");
+console.log("connect to PS at localhost:3100");
+
+var res1 = {};
+let tsKey = [];
+tsName = [];
+tsIn = [];
+linkHash = "";
+
+let i = 0;
+let j = 0;
+let k = 0;
+
+for (i = 0; i < 1000; i++) {
+	k = 1000 + i;
+	j = "" + k;
+	tsName[i] = j;
+	tsKey[i] = crypto.createHash("sha256").update(j).digest("hex");
+	tsIn[i] = " ";
+}
+
+async function intervalFunc() {
+	last = "submitted"
+	for (i = 1; i < 1000; i++) {
+		if (last == "submitted") {
+			tsId = tsName[i];
+			chainId = tsKey[i];
+			tsKey[i] = crypto.createHash("sha256").update(chainId).digest("hex");
+			tHash = tsKey[i];
+		}
+
+		const data = { tsId, chainId, tHash };
+		const options = {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(data)
+		};
+		res1 = await fetch("http://127.0.0.1:3100/hashFile", options);
+		const disp = await res1.json();
+		// console.log("send data json to PS")
+		// console.log(data)
+		// console.log("ready for next, completed sending to PS ");
+		// console.log(disp);
+	}
+}
+
+setInterval(intervalFunc, 1005);
